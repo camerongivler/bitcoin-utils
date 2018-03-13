d7d81353e4b060f12821431b1c09703d0dff0f6a
aweeaton <aweeaton@gmail.com>
2018-03-13 19:08:36 -0400

    ExchangeBase class now has exchange method

--- exchangebase.py
+++ exchangebase.py
@@ -8,3 +8,8 @@ class ExchangeBase:
     def getName(self):
         pass
 
+    def exchange(sellWallet, buyWallet, amount, rate, fee):
+        if sellWallet.amount < amount: return False
+        sellWallet.amount -= amount
+        buyWallet.amount += amount * (1-fee) * rate
+        return True
