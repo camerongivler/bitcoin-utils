e96f3a2dfe73bae6ddf8b92db9125d680f76b22d
Cameron Givler <cameron@aqueti.com>
2018-03-13 22:17:23 -0400

    handle kraken errors

--- krakenapi.py
+++ krakenapi.py
@@ -7,10 +7,14 @@ class Kraken(ExchangeBase):
     k = KrakenAPI(api)
 
     def getLastTradePrice(self, symbol):
-        mySymbol = symbol.replace("BTC", "XBT").replace("-","")
-        krakenTicker = self.k.get_ticker_information(mySymbol)
-        # c = last trade
-        return float(krakenTicker['c'][0][0])
+        try:
+            mySymbol = symbol.replace("BTC", "XBT").replace("-","")
+            krakenTicker = self.k.get_ticker_information(mySymbol)
+            # c = last trade
+            return float(krakenTicker['c'][0][0])
+        except requests.exceptions.ConnectionError:
+            sleep(3)
+            return self.getLastTradePrice(symbol)
 
     def getName(self):
         return "kraken"
