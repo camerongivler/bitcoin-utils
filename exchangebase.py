689481f6faf47cb8da118fec15088351e4cbb897
aweeaton <aweeaton@gmail.com>
2018-03-14 19:28:00 -0400

    added variables that hold wallets

--- exchangebase.py
+++ exchangebase.py
@@ -1,5 +1,12 @@
 class ExchangeBase:
 
+    #Create variables that are dictionaries that hold all wallets for a certain exchange
+    krakenWallets = {}
+    geminiWallets = {}
+    gdaxWallets = {}
+    
+    #Future wallets go here as we add more exchanges
+    
     # The symbols will be taken in GDAX form (BTC-USD) and converted appropriately
     def getLastTradePrice(self, symbol):
         pass
@@ -7,7 +14,9 @@ class ExchangeBase:
     # The name of the exchange
     def getName(self):
         pass
-
+    
+    #exchange method that takes money from the sellWallet and adds
+    #it to the buy wallet taking out the fee and multiplied by the rate
     def exchange(sellWallet, buyWallet, amount, rate, fee):
         if sellWallet.amount < amount: return False
         sellWallet.amount -= amount
