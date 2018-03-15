c5f1ee4ac51ced6c566afcf04542273552ef3a57
aweeaton <aweeaton@gmail.com>
2018-03-15 11:44:46 -0400

    Exchange class deleted

--- wallet.py
+++ wallet.py
@@ -6,14 +6,3 @@ class Wallet:
         self.exchange = exchange
         self.currency = currency
         self.amount = amount
-
-
-class Exchange:
-
-    # Takes [amount] out of [sellWallet], mutliplies it by [rate]
-    # and deposits it into [buyWallet], fee is decimal percent
-    def exchange(sellWallet, buyWallet, amount, rate, fee):
-        if sellWallet.amount < amount: return False
-        sellWallet.amount -= amount
-        buyWallet.amount += amount * (1-fee) * rate
-        return True
