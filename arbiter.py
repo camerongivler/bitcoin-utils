9ef521330c55c7018154f9fb560ba590eb8f2980
Cameron Givler <cameron@aqueti.com>
2018-03-21 00:07:07 -0400

    better diff

--- arbiter.py
+++ arbiter.py
@@ -26,7 +26,7 @@ for exchange in exchanges.values():
 
 cutoff = 1.42 # %  - this will guarentee 0.2% per trade
 #cutoff = 0 # for testing
-runningAverage = 0.2 #keep track of the running average over the past ~2 hours
+runningAverage = 0.15 #keep track of the running average over the past ~2 hours
 
 trades=[]
 #First trade loses money, but gets the ball rolling
