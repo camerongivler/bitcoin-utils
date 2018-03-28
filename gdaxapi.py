a36eb305f8da9339f4560f3eba6250b0fb2b20a6
Cameron Givler <cameron@aqueti.com>
2018-03-28 00:07:44 -0400

    LTC

--- gdaxapi.py
+++ gdaxapi.py
@@ -7,12 +7,12 @@ class Gdax(ExchangeBase):
 
     def __init__(self):
         super().__init__()
-        self.wallets["LTC"] = Wallet("LTC", 0)
+        self.wallets["LTC"] = Wallet("LTC", 3.729395)
         self.wallets["ETH"] = Wallet("ETH", 0)
-        self.wallets["BCH"] = Wallet("BCH", 0.5607267)
+        self.wallets["BCH"] = Wallet("BCH", 0)
         self.wallets["BTC"] = Wallet("BTC", 0)
         self.wallets["USD"] = Wallet("USD", 0)
-        self.valueWallet = self.wallets["BCH"]
+        self.valueWallet = self.wallets["LTC"]
 
         self.fee = 0.0025
 
